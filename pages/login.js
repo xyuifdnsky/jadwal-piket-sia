8d2acee5e99e26d6c7379daa82205d6a5192853b
xyuifdnsky <ranggaagungfadilah@gmail.com>
2026-02-05 15:23:51 +0700

    update ui

--- pages/login.js
+++ pages/login.js
@@ -1,30 +1,12 @@
-import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../lib/firebase";
-import { useRouter } from "next/router";
 
-export default function Login() {
-  const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const login = async () => {
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-      router.push("/generate_jadwal");
-    } catch {
-      alert("Login gagal");
-    }
-  };
-
-  return (
-    <div style={{ padding: 40 }}>
-      <h2>Login Admin</h2>
-      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-      <br />
-      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-      <br />
-      <button onClick={login}>Login</button>
-    </div>
-  );
-}
+const handleLogin = async () => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    router.push("/");
+  } catch (err) {
+    console.error("LOGIN ERROR:", err.code, err.message);
+    alert(err.code);
+  }
+};
