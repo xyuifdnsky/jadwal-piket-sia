d5ce1a22f42f0b8d96259beb1bc40064b4134fc3
xyuifdnsky <ranggaagungfadilah@gmail.com>
2026-02-05 14:51:33 +0700

    update ui

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="id">
+      <Head>
+        {/* 🔥 WAJIB AGAR RESPONSIVE DI HP */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
